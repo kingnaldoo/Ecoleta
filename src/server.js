8956dc4e821d1065afdd97cf991b6755ef390806
kingnaldoo <reinaldosn2016@gmail.com>
2020-11-27 15:00:26 -0300

    Structuring the server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.send('')
+})
+
+
+app.listen(3000, () => {
+    console.log('O servidor está rodando😎')
+})
