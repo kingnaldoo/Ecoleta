94db49a0102720afb21df4bd7abccff75634ea73
kingnaldoo <reinaldosn2016@gmail.com>
2020-11-28 10:04:39 -0300

    Configuring routes and rewritting views archives for nunjucks

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
 const app = express()
 
 app.use(express.static('public'));
 
+nunjucks.configure(__dirname+'/views', {
+    autoescape: true, 
+    express: app
+});
+
 app.get('/', (req, res)=>{
-    res.send('')
+    res.render('index.html')
 })
 
+app.get('/create-point', (req, res)=>{
+    res.render('create-point.html')
+})
+
+app.get('/search-results', (req, res)=>{
+    res.render('search-results.html')
+})
 
 app.listen(3000, () => {
     console.log('O servidor está rodando😎')
